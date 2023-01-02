de6a574d73771731292206ea52f527a1203dcf71
Vasily_Suchkov <MrGetFlash@yandex.ru>
2023-01-02 16:40:18 +0700

    тестируем обратный калькулятор, версия 2

--- Test/ReversePolishNotationCalculatorTest.java
+++ Test/ReversePolishNotationCalculatorTest.java
@@ -8,28 +8,24 @@ public class ReversePolishNotationCalculatorTest {
     @Test
     public void shouldCalculateAddition() {
         int i = calculator.calculatePolishNotation("10 1 +");
-        //System.out.println(i);
         Assert.assertEquals(11, i);
     }
 
     @Test
     public void shouldCalculateSubtraction() {
         int i = calculator.calculatePolishNotation("10 5 -");
-        //System.out.println(i);
         Assert.assertEquals(5, i);
     }
 
     @Test
     public void shouldCalculateMultiplication() {
         int i = calculator.calculatePolishNotation("10 5 3 * - ");
-        //System.out.println(i);
         Assert.assertEquals(-5, i);
     }
 
     @Test
     public void shouldGiveLastValueAndTestPartsIsBlank() {
         int i = calculator.calculatePolishNotation("1  10 5 3");
-        System.out.println(i);
         Assert.assertEquals(3, i);
     }
 
